373c1c703208e356635f146b5a6e0fa84af10211
Sumitisme <scalarscam2004@gmail.com>
2024-05-18 20:42:22 +0545

    Uploading the files

---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,52 @@
+return {
+  {
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+  },
+  config = function()
+    local dap = require("dap")
+    local dapui = require("dapui")
+
+    dapui.setup()
+
+    dap.listeners.after.event_initialized["dapui_config"] = function()
+      dapui.open()
+    end
+
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+
+    vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})
+    vim.keymap.set("n", "<leader>dc", dap.continue, {})
+    vim.keymap.set("n", "<leader>dx", ':DapTerminate<CR>')
+    vim.keymap.set("n", "<leader>do", ':DapStepOver<CR>')
+
+  end,
+  },
+  -- the following is for codelldb
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
+    },
+    opts = {
+      handlers = {},
+      ensure_installed = {
+        "codelldb",
+      }
+    }
+  }
+}
---
+++ lua/plugins/none-ls-extras.lua
@@ -0,0 +1,15 @@
+return {
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				require("none-ls.code_actions.eslint"),
+			},
+		})
+	end,
+}
